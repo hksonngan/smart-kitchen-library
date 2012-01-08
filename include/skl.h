f729157541f62be13b15533d2860c2bdaf833801
ahashimoto1009@gmail.com <ahashimoto1009@gmail.com@0593f14f-b524-2927-4e14-da923324354e>
2012-01-08 08:49:32 +0000

    delete some old files and add header files.

---
+++ include/skl.h
@@ -0,0 +1,6 @@
+#include "Core/include/sklutils.h"
+#include "Core/include/OptParser.h"
+#include "Core/include/sklstring.h"
+#include "Core/include/sklTime.h"
+#include "Core/include/graph.h"
+#include "Core/include/StopWatch.h"

a40b866c0caba7e66007481734d54c458e4841ea
ahashimoto1009@gmail.com <ahashimoto1009@gmail.com@0593f14f-b524-2927-4e14-da923324354e>
2012-05-31 05:30:27 +0000

    A common interface for sensor data capturing modules

---
+++ Core/include/SensorModuleBase.h
@@ -0,0 +1,48 @@
+﻿/*!
+ * @file SensorModuleBase.h
+ * @author a_hasimoto
+ * @date Date Created: 2012/May/31
+ * @date Last Change:2012/May/31.
+ */
+#ifndef __SKL_SENSOR_MODULE_BASE_H__
+#define __SKL_SENSOR_MODULE_BASE_H__
+
+
+namespace skl{
+
+/*!
+ * @class SensorModuleBase
+ * @brief センサ情報の取得を行うモジュールが継承すべき既定クラス
+ */
+template <class SensorOutput,class SensorIdentifier=std::string> class SensorModuleBase{
+	public:
+		SensorModuleBase();
+		virtual ~SensorModuleBase();
+		virtual bool open(const SensorIdentifier& sensor_identity)=0;/// センサを特定する情報を入力する
+		virtual void release()=0;// センサとの接続を着る
+		virtual bool isOpened()const=0;
+		virtual bool grab()=0;
+		virtual bool retrieve(SensorOutput& block,int channel=0)=0;
+		virtual size_t size()const=0;
+	protected:
+		
+	private:
+};
+
+
+/*!
+ * @brief デフォルトコンストラクタ
+ */
+template<class SensorOutput,class SensorIdentifier> SensorModuleBase<SensorOutput,SensorIdentifier>::SensorModuleBase(){
+
+}
+
+/*!
+ * @brief デストラクタ
+ */
+template<class SensorOutput,class SensorIdentifier> SensorModuleBase<SensorOutput,SensorIdentifier>::~SensorModuleBase(){
+}
+
+} // skl
+#endif // __SKL_SENSOR_MODULE_BASE_H__
+
